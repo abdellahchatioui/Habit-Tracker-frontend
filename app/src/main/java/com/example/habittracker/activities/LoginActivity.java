3273fd4135019649effd5fe138a69d5332a30e9c
abdellahchatioui <abdellahchatioui123@gmail.com>
2026-02-18 22:33:33 +0000

    Add Auth feat

--- app/src/main/java/com/example/habittracker/activities/LoginActivity.java
+++ app/src/main/java/com/example/habittracker/activities/LoginActivity.java
@@ -1,6 +1,10 @@
 package com.example.habittracker.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,15 +16,30 @@ import com.example.habittracker.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        EditText etEmail = findViewById(R.id.etEmail);
+        EditText etPassword = findViewById(R.id.etPassword);
+
+        Button btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(v->{
+            String email = etEmail.getText().toString();
+            String password = etPassword.getText().toString();
+
+            if(email.isEmpty() || password.isEmpty()){
+                Toast.makeText(this, "Fill all fields", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
         });
     }
 }
