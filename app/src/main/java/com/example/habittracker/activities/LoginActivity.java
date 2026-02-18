fa710d186eee31e8d809e94520c5e44ffd87ba24
abdellahchatioui <abdellahchatioui123@gmail.com>
2026-02-18 23:05:41 +0000

    Add Volley deps,And connect auth feat with backend using api

--- app/src/main/java/com/example/habittracker/activities/LoginActivity.java
+++ app/src/main/java/com/example/habittracker/activities/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.example.habittracker.activities;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,7 +13,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.android.volley.Request;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.habittracker.R;
+import com.example.habittracker.network.VolleySingleton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -29,17 +37,54 @@ public class LoginActivity extends AppCompatActivity {
         Button btnLogin = findViewById(R.id.btnLogin);
 
         btnLogin.setOnClickListener(v->{
-            String email = etEmail.getText().toString();
-            String password = etPassword.getText().toString();
+            String email = etEmail.getText().toString().trim();
+            String password = etPassword.getText().toString().trim();
 
             if(email.isEmpty() || password.isEmpty()){
                 Toast.makeText(this, "Fill all fields", Toast.LENGTH_SHORT).show();
                 return;
             }
 
-            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-            startActivity(intent);
-            finish();
+            String url = "http://10.0.2.2:8080/api/auth/login";
+
+            JSONObject jsonObject = new JSONObject();
+            try {
+                jsonObject.put("email",email);
+                jsonObject.put("password",password);
+            }catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            JsonObjectRequest request = new JsonObjectRequest(
+                    Request.Method.POST,
+                    url,
+                    jsonObject,
+                    response -> {
+                        try {
+                            String token = response.getString("token");
+
+                            saveToken(token);
+
+                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            startActivity(intent);
+                            finish();
+                        } catch (JSONException e){
+                            e.printStackTrace();
+                        }
+                    },
+                    error -> {
+                        Toast.makeText(LoginActivity.this,
+                                "Login failed",
+                                Toast.LENGTH_SHORT).show();
+                    }
+            );
+            VolleySingleton.getInstance(this).getRequestQueue().add(request);
         });
+    }
+    private void saveToken(String token){
+        SharedPreferences prefs = getSharedPreferences("app_prefs",MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString("jwt_token",token);
+        editor.apply();
     }
 }
---
+++ app/src/main/java/com/example/habittracker/network/VolleySingleton.java
@@ -0,0 +1,30 @@
+package com.example.habittracker.network;
+
+import android.content.Context;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+public class VolleySingleton {
+    private static VolleySingleton instance;
+    private RequestQueue requestQueue;
+    private static Context context;
+
+    private VolleySingleton(Context context){
+        context = context;
+        requestQueue = getRequestQueue();
+    }
+
+    public static synchronized VolleySingleton getInstance(Context context){
+        if(instance == null){
+            instance = new VolleySingleton(context);
+        }
+        return instance;
+    }
+    public RequestQueue getRequestQueue(){
+        if(requestQueue == null){
+            requestQueue = Volley.newRequestQueue(context.getApplicationContext());
+        }
+        return requestQueue;
+    }
+}
