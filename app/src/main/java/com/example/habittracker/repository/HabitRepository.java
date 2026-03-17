2e9f43ccd608bc87570a3947e95a3bda376e8beb
abdellahchatioui <abdellahchatioui123@gmail.com>
2026-03-17 18:02:29 +0000

    refactor: Move UPDATE to Repository

--- app/src/main/java/com/example/habittracker/repository/HabitRepository.java
+++ app/src/main/java/com/example/habittracker/repository/HabitRepository.java
@@ -6,10 +6,12 @@ import android.content.SharedPreferences;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.example.habittracker.network.VolleySingleton;
 import com.example.habittracker.utils.AppConstants;
 
 import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -62,4 +64,37 @@ public class HabitRepository {
                 .getRequestQueue()
                 .add(request);
     }
+
+    public void updateHabit(
+            Context context,
+            Long habitId,
+            JSONObject data,
+            Response.Listener<JSONObject> listener,
+            Response.ErrorListener errorListener
+    ) {
+
+        String url = AppConstants.HABIT_URL + "/" + habitId;
+
+        JsonObjectRequest request = new JsonObjectRequest(
+                Request.Method.PUT,
+                url,
+                data,
+                listener,
+                errorListener
+        ) {
+            @Override
+            public Map<String, String> getHeaders() {
+                SharedPreferences prefs =
+                        context.getSharedPreferences("app_prefs", Context.MODE_PRIVATE);
+
+                Map<String, String> headers = new HashMap<>();
+                headers.put("Authorization", "Bearer " + prefs.getString("jwt_token", ""));
+                headers.put("Content-Type", "application/json");
+                return headers;
+            }
+        };
+
+        VolleySingleton.getInstance(context).getRequestQueue().add(request);
+    }
+
 }
