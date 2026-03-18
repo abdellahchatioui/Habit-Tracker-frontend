fc8d7d8693c844e2d4a43bb6577d0874cd32937c
abdellahchatioui <abdellahchatioui123@gmail.com>
2026-03-18 21:47:17 +0000

    fix: fix the backButton buy enlarge the image size

--- app/src/main/java/com/example/habittracker/utils/AppConstants.java
+++ app/src/main/java/com/example/habittracker/utils/AppConstants.java
@@ -1,7 +1,7 @@
 package com.example.habittracker.utils;
 
 public class AppConstants {
-    public static final String BASE_URL = "http://172.20.10.3:8080";
+    public static final String BASE_URL = "http://192.168.31.5:8080";
     public static final String LOGIN_URL = BASE_URL + "/api/auth/login";
     public static final String REGISTER_URL = BASE_URL + "/api/auth/register";
     public static final String HABIT_URL = BASE_URL + "/api/habits";
